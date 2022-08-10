72c86b82a9707f4d44f93dee51fb60b39f2655f7
Victor Prete <vprete@tuta.io>
2022-08-10 16:04:12 -0400

    Add show page to flats

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,9 +1,33 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: %i[show]
+
   def index
     @flats = Flat.all
     @images = []
     10.times do
       @images << 'https://source.unsplash.com/random/?house'
     end
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.create(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :description, :number_of_guests, :price_per_night, :address)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:id])
   end
 end
